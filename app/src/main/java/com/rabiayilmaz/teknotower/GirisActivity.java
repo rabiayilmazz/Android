87e262350ba42e73110ff0aeb95c0e1515eed1ca
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-01-01 15:38:28 +0300

    Giriş activity

--- app/src/main/java/com/rabiayilmaz/teknotower/GirisActivity.java
+++ app/src/main/java/com/rabiayilmaz/teknotower/GirisActivity.java
@@ -1,14 +1,60 @@
 package com.rabiayilmaz.teknotower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class GirisActivity extends AppCompatActivity {
 
+
+    EditText kullaniciAdEditText;
+    Button kayitOlButton;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris);
+        tanimla();
+    }
+    public void tanimla(){
+        kullaniciAdEditText = (EditText)findViewById(R.id.kullaniciAdEditText);
+        kayitOlButton = (Button)findViewById(R.id.kayitOlButton);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
+
+        kayitOlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = kullaniciAdEditText.getText().toString();
+                kullaniciAdEditText.setText("");
+                ekle(username);
+            }
+        });
+
+    }
+    public void ekle(final String kadi){
+        reference.child("Kullanıcılar").child(kadi).child("kullaniciadi").setValue(kadi).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Başarılı bir şekilde giriş yapılmıştır.",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(GirisActivity.this,MainActivity.class);
+                    intent.putExtra("kadi", kadi);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
